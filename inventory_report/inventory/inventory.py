749ad69fb1bdc4fe549fc74f54f7876cc0c2b560
Juliane <julianesilvac75@gmail.com>
2022-11-06 19:23:01 +0000

    refactor: import_data method for Inventory class

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -8,31 +8,45 @@ from inventory_report.reports.complete_report import CompleteReport
 class Inventory:
     @classmethod
     def import_data(cls, file, type):
-        product_list = []
-
-        if file.endswith("csv"):
-            with open(file) as csv_file:
-                products = csv.DictReader(csv_file, delimiter=",", quotechar='"')
-
-                product_list = [row for row in products]
+        if file.endswith(".csv"):
+            products_list = cls.get_csv_file(file)
 
         if file.endswith("json"):
-            with open(file) as json_file:
-                products = json.load(json_file)
-                product_list = products
+            products_list = cls.get_json_file(file)
 
         if file.endswith("xml"):
-            with open(file) as xml_file:
-                products = xmltodict.parse(xml_file.read())
-                xml_file.close()
-                product_list = products["dataset"]["record"]
+            products_list = cls.get_xml_file(file)
 
         if type == "simples":
-            return SimpleReport.generate(product_list)
+            return SimpleReport.generate(products_list)
         elif type == "completo":
-            return CompleteReport.generate(product_list)
+            return CompleteReport.generate(products_list)
         else:
             raise ValueError
+
+    @classmethod
+    def get_csv_file(cls, file):
+        with open(file) as csv_file:
+            reader = csv.DictReader(csv_file, delimiter=",", quotechar='"')
+            products_list = [row for row in reader]
+
+        return products_list
+
+    @classmethod
+    def get_json_file(cls, file):
+        with open(file) as json_file:
+            products_list = json.load(json_file)
+
+        return products_list
+
+    @classmethod
+    def get_xml_file(cls, file):
+        with open(file) as xml_file:
+            products = xmltodict.parse(xml_file.read())
+            xml_file.close()
+            products_list = products["dataset"]["record"]
+
+        return products_list
 
 
 if __name__ == "__main__":
