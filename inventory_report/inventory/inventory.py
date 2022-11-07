60d0b540b2b3987cd4e4b4b66b410e2826dbd350
Juliane <julianesilvac75@gmail.com>
2022-11-07 00:39:24 +0000

    revert commit: import_data is not working anymore

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,10 +1,5 @@
 import csv
 import json
-import xmltodict
-
-# from inventory_report.importer.csv_importer import CsvImporter
-# from inventory_report.importer.json_importer import JsonImporter
-# from inventory_report.importer.xml_importer import XmlImporter
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
@@ -16,21 +11,15 @@ class Inventory:
 
         if file.endswith("csv"):
             with open(file) as csv_file:
-                products = csv.DictReader(
-                    csv_file, delimiter=",", quotechar='"'
-                )
-                product_list = [row for row in products]
+                reader = csv.DictReader(csv_file, delimiter=",", quotechar='"')
+
+                product_list = [row for row in reader]
 
         if file.endswith("json"):
             with open(file) as json_file:
                 products = json.load(json_file)
-                product_list = products
 
-        if file.endswith("xml"):
-            with open(file) as xml_file:
-                products = xmltodict.parse(xml_file.read())
-                xml_file.close()
-                product_list = products["dataset"]["record"]
+            product_list = products
 
         if type == "simples":
             return SimpleReport.generate(product_list)
@@ -41,4 +30,4 @@ class Inventory:
 
 
 if __name__ == "__main__":
-    Inventory.import_data("inventory_report/data/inventory.xml", "simples")
+    Inventory.import_data("inventory_report/data/inventory.csv", "simples")
