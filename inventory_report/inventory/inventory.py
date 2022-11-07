a991839b406f5577f96a92c13a01e9e5d69c5d41
Juliane <julianesilvac75@gmail.com>
2022-11-07 00:45:41 +0000

    bug: trying to fix the bug

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,5 @@
 import csv
+import json
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
@@ -6,10 +7,19 @@ from inventory_report.reports.complete_report import CompleteReport
 class Inventory:
     @classmethod
     def import_data(cls, file, type):
-        with open(file) as csv_file:
-            reader = csv.DictReader(csv_file, delimiter=",", quotechar='"')
+        product_list = []
 
-            product_list = [row for row in reader]
+        if file.endswith("csv"):
+            with open(file) as csv_file:
+                reader = csv.DictReader(csv_file, delimiter=",", quotechar='"')
+
+                product_list = [row for row in reader]
+
+        if file.endswith("json"):
+            with open(file) as json_file:
+                products = json.load(json_file)
+
+            product_list = products
 
         if type == "simples":
             return SimpleReport.generate(product_list)
