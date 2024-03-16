69fe58ac54994b9cb8f0591a61e3b8bba4c14880
Juliane Cardoso <julianesilvac75@gmail.com>
2024-03-16 15:49:27 +0000

    final

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,25 +1,58 @@
 import csv
 import json
+import xmltodict
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
 
 class Inventory:
     @classmethod
-    def import_data(cls, file, type):
+    def get_csv_file(cls, file):
+        with open(file) as csv_file:
+            reader = csv.DictReader(csv_file, delimiter=",", quotechar='"')
+
+            product_list = [row for row in reader]
+
+        return product_list
+
+    @classmethod
+    def get_json_file(cls, file):
+        with open(file) as json_file:
+            products = json.load(json_file)
+
+        return products
+
+    @classmethod
+    def get_xml_file(cls, file):
+        if file.endswith("xml") is False:
+            raise ValueError("Arquivo inválido")
+
+        with open(file) as xml_file:
+            products = xmltodict.parse(xml_file.read())
+            xml_file.close()
+            products_list = products["dataset"]["record"]
+
+        return products_list
+
+    @classmethod
+    def get_products(cls, file):
         product_list = []
 
         if file.endswith("csv"):
-            with open(file) as csv_file:
-                reader = csv.DictReader(csv_file, delimiter=",", quotechar='"')
-
-                product_list = [row for row in reader]
-
+            product_list = cls.get_csv_file(file)
         if file.endswith("json"):
-            with open(file) as json_file:
-                products = json.load(json_file)
+            product_list = cls.get_json_file(file)
+        else:
+            with open(file) as xml_file:
+                products = xmltodict.parse(xml_file.read())
+                xml_file.close()
+                product_list = products["dataset"]["record"]
 
-            product_list = products
+        return product_list
+
+    @classmethod
+    def import_data(cls, file, type):
+        product_list = cls.get_products(file)
 
         if type == "simples":
             return SimpleReport.generate(product_list)
